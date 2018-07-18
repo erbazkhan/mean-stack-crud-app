379972db98f15ae1c540f3033cf0fee9c07173a8
Erbaz Khan <41143707+ErbazKhan@users.noreply.github.com>
2018-07-18 14:40:02 +0500

    Add files via upload

---
+++ NewServer.js
@@ -0,0 +1,272 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongo = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectID;
+const Assert = require('assert');
+//const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const expressHandlebars = require('express-handlebars');
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const User = require('./models/user.js');
+const Admin = require('./models/admin');
+
+
+app.engine('handlebars', expressHandlebars({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+const port = 8888;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//Connect Flash
+app. use(flash());
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg  =req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use(express.static(__dirname)); // For Loading all the files present in Directory
+
+mongoose.Promise = global.Promise;
+const url = 'mongodb://localhost:27017/userDB';
+mongoose.connect(url);
+
+//============================ HOME =====================================
+app.get("/", function (req, res) {
+    const homePath = path.join(__dirname, '/home.html');
+    res.sendFile(homePath);
+});
+
+//************************* A D M I N ***********************************
+
+//========================== SIGN-UP ====================================
+app.get("/signup", function (req, res) {
+    const signUpPath = path.join(__dirname, '/sign-up.html');
+    res.sendFile(signUpPath);
+});
+function SignUpCallback(req, res){
+    // if (req.body.adminUsername && req.body.adminEmail && req.body.password && req.body.passwordconf) {
+    //     const adminData = new Admin(req.body);
+    //     console.log(adminData);
+    //     adminData.save()
+    //         .then(admin => {
+    //         res.send("Admin registered!").redirect('/login');
+    //         })
+    // }
+    // else
+    //     res.status(400).send('Unable to register, Try Again!');
+    const adminUsername = req.body.adminUsername;
+    const adminEmail = req.body.adminEmail;
+    const password = req.body.password;
+    const passwordconf = req.body.passwordconf;
+
+    req.checkBody('adminUsername', 'adminUsername is required').notEmpty();
+    req.checkBody('adminEmail', 'adminEmail is required').notEmpty();
+    req.checkBody('adminEmail', 'adminEmail is not valid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('passwordconf', 'Passwords do not match').equals(req.body.password);
+
+
+    var error = req.validationErrors();
+
+    if (error) {
+        res.send(error);
+        res.redirect('/signup');
+    } else {
+        const newAdmin = new Admin({
+            adminUsername: adminUsername,
+            adminEmail: adminEmail,
+            password: password
+        });
+        console.log('hey')
+        console.log(newAdmin);
+
+        Admin.createAdmin(newAdmin);
+        //res.json('You are registered and can now login!')
+        res.redirect('/login');
+    }
+}
+app.post('/signup', SignUpCallback);
+
+//========================== LOGIN =================================
+app.get("/login", function (req, res) {
+    const loginPath = path.join(__dirname, '/login.html');
+    res.sendFile(loginPath);
+});
+
+passport.use(new localStrategy(
+    function (adminUsername, password, done) {
+        Admin.getAdminByAdminUsername(adminUsername, function (err, admin) {
+            if (err) throw err;
+            console.log('getAdmin called');
+            if (!admin) {
+                console.log('Not admin');
+                return done(null, false, function(req, res)
+                {
+                    console.log('Missing credentials!');
+                });
+            }
+
+            Admin.comparePassword(password, admin.password, function (err, isMatch) {
+                console.log('comparePassword called');
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, admin);
+                } else {
+                    return done(null, false, function(req, res)
+                    {
+                        console.log('Missing credentials!');
+                    });
+                }
+            });
+        });
+    }));
+
+passport.serializeUser(function (admin, done) {
+    done(null, admin.id);
+});
+passport.deserializeUser(function (id, done) {
+    Admin.getAdminById(id, function (err, admin) {
+        done(err, admin);
+        console.log('findById called');
+    });
+});
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+        failureRedirect: '/login'}),
+    function(req, res){
+        if (failureRedirect) console.log("login");
+
+        console.log('login called');
+        res.redirect('/');
+    }
+);
+
+//************************** C. R. U. D. ********************************
+
+//===============================POST====================================
+app.get("/adduser", function (req, res) {
+    const homePath = path.join(__dirname, '/adduser.html');
+    res.sendFile(homePath);
+});
+
+function PostCallback(req, res) {
+    const userData = new User(req.body);
+    //console.log(userData);
+
+    userData.save()
+        .then(item => {
+            res.send("Item saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save data to DB");
+        });
+}
+app.post("/adduser", PostCallback);
+
+//===============================GET=====================================
+app.get("/users", function(req, res) {
+    const result = [];
+    mongo.connect(url, function(err, db){
+        assert.equal(null, err);
+        const dbo = db.db('userDB');
+        const allData = dbo.collection('users').find({});
+        allData.forEach(function(doc, err) {
+            assert.equal(null, err);
+            result.push(doc);
+        }, function () {
+            //console.log(result);
+            db.close();
+            res.json(allData);
+        });
+    });
+});
+
+//=============================DELETE====================================
+app.get("/delete", function (req, res) {
+    const deletePath = path.join(__dirname, '/delete.html');
+    res.sendFile(deletePath);
+});
+function deleteCallback(req, res) {
+    mongo.connect(url, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("userDB");
+        const myQuery = req.body;
+        console.log(myQuery);
+        dbo.collection("users").deleteOne({user_name: myQuery.user_name}, function (err) {
+            if (err) throw err;
+            else
+                res.send("item deleted from database!");
+        });
+    });
+}
+app.post('/delete', deleteCallback);
+
+//=============================UPDATE====================================
+app.get("/update", function (req, res) {
+    const updatePath = path.join(__dirname, '/update.html');
+    res.sendFile(updatePath);
+});
+function updateCallback(req, res) {
+    const item = {
+        user_name : req.body.user_name,
+        user_mail : req.body.user_mail,
+        user_phone : req.body.user_phone
+    };
+    const ID = req.body._id;
+    console.log(item);
+
+    const id = objectId(ID);
+    console.log(id);
+
+    mongo.connect(url, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("userDB");
+        dbo.collection('users').updateOne({_id: objectId(ID)}, {$set: item}, function (err) {
+            if (err) throw err;
+            else
+                res.send("Item updated in database!");
+        });
+    });
+}
+app.post('/update', updateCallback);
+
+//=======================================================================
+app.listen(8888);
+console.log("Running at port 8888...");
